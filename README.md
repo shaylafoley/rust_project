41a560972dffd6b49e59163657de61a1d5b0217e
Shayla Foley <shaylafoley@Mac.lan>
2025-11-20 14:51:55 -0500

    Hangman Game

---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+
+This is a program in Rust I have created.  The purpose of this project is to become aquanited with the Rust language.  This program is to learn a new skill and show the ability I have to learn new things and a willingness to learn new skills.
+
+This program in a game like Hangman.  I will use variables, expressions, conditionals, loops, functions and data structure.  You will be able to guess a word.
+
+The purpose in writing this software is to add to my software skills.  Rust is a lanugage I am not familiar with and wanted to expand the area of my programming skills.  
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+Visual Studio Code - rust-analyzer
+
+Cargo
+
+Rust Language
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Rust Tutorial Full Course](https://www.youtube.com/watch?v=ygL_xcavzQ4)
+- [Rust](https://rust-lang.org)
+- [Wikepedia Rust](https://en.wikipedia.org/wiki/Rust_(programming_language))
+
+# Future Work
+
+- Create more variety of words to guess
+- Program more error handling
+- Create time limits or a countdown
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let words = ["apple", "banana", "orange", "grape", "melon"];
+
+    println!("Welcome to Hangman!");
+
+    // Pick a word manually by index 
+    let secret = words[3]; // <-- change this number to pick a different word
+    let secret_chars: Vec<char> = secret.chars().collect();
+
+    let mut guessed: Vec<char> = Vec::new();
+    let mut wrong_guesses = 0;
+    let max_wrong = 6;
+
+    loop {
+        println!("\nWord: {}", masked_word(&secret_chars, &guessed));
+        println!("Wrong guesses: {}/{}", wrong_guesses, max_wrong);
+
+        // Check lose
+        if wrong_guesses >= max_wrong {
+            println!("You lost! The word was: {}", secret);
+            break;
+        }
+
+        // Check win
+        if secret_chars.iter().all(|c| guessed.contains(c)) {
+            println!("You won! The word was: {}", secret);
+            break;
+        }
+
+        // Get input
+        print!("Enter a letter: ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        let input = input.trim().to_lowercase();
+
+        if input.len() != 1 {
+            println!("Please enter exactly one letter.");
+            continue;
+        }
+
+        let ch = input.chars().next().unwrap();
+
+        if guessed.contains(&ch) {
+            println!("You already guessed '{}'", ch);
+            continue;
+        }
+
+        guessed.push(ch);
+
+        if !secret_chars.contains(&ch) {
+            wrong_guesses += 1;
+            println!("'{}' is not in the word.", ch);
+        } else {
+            println!("Good guess!");
+        }
+    }
+}
+
+fn masked_word(secret: &[char], guessed: &[char]) -> String {
+    secret
+        .iter()
+        .map(|c| if guessed.contains(c) { *c } else { '_' })
+        .collect()
 }
